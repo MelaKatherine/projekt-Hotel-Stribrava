9392510a088c9187974bf40cec37189d6d75837b
Katerina <kdatsenka@gmail.com>
2025-05-24 15:51:52 +0200

    add Order and Detail with css styles

--- src/componenty/RoomItem/RoomItem.jsx
+++ src/componenty/RoomItem/RoomItem.jsx
@@ -1,12 +1,10 @@
 import './RoomItem.css';
 export const RoomItem = ({ name, price, image }) => {
   return (
-    <div className="cards-row">
-      <div className="card">
-        <img className="card__image" src={image} />
-        <div className="card__title">{name}</div>
-        <div className="card__body">{price} Kč na osobu</div>
-      </div>
+    <div className="card">
+      <img className="card__image" src={image} />
+      <div className="card__title">{name}</div>
+      <div className="card__body">{price} Kč na osobu</div>
     </div>
   );
 };
--- src/componenty/Rooms/Rooms.jsx
+++ src/componenty/Rooms/Rooms.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { RoomItem } from '../RoomItem/RoomItem';
+import { Detail } from '../Detail/Detail';
+import { Order } from '../Order/Order';
 import './Rooms.css';
 export const Rooms = () => {
   const [rooms, setRooms] = useState([]);
@@ -20,11 +22,19 @@ export const Rooms = () => {
         <h2>Heading</h2>
         <p>Quas odio quidem, enim nihil unde quia temporibus vitae in ab.</p>
         <div className="cards-row">
-          {console.log(rooms)}
           {rooms.map((room) => (
             <RoomItem name={room.name} price={room.price} image={room.image} />
           ))}
         </div>
+        <section class="light">
+          <div class="container">
+            <h2>Heading</h2>
+            <div class="columns-2">
+              <Detail />
+              <Order />
+            </div>
+          </div>
+        </section>
       </div>
     </section>
   );
